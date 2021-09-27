373380ef260bd1a886adf618334fd27dc55496a5
sagar <sagar.wgl17@gmail.com>
2021-09-28 01:54:00 +0530

    'added'

--- one.py
+++ one.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
+from pyspark.sql.functions import  *
+from pyspark.sql.types import StructField,StructType,StringType,LongType
 
 spark = SparkSession.builder.appName("structured_operations")\
     .master("local[*]")\
@@ -12,4 +13,25 @@ spark.conf.set("spark.sql.shuffle.partition","5")
 df = spark.read.format("json").load(r"F:\SPARK_DEFINITIVE_PROJECTS\\data\Spark-The-Definitive-Guide\data\flight-data\json\2015-summary.json")
 
 
-df.printSchema()
+# df.printSchema()
+
+
+# manual schema
+
+myManualSchema = StructType([
+    StructField("DEST_COUNTRY_NAME",StringType(),True),
+    StructField("ORIGIN_COUNTRY_NAME",StringType(),True),
+    StructField("count",LongType(),True)
+])
+
+df = spark.read.format("json").schema(myManualSchema).load(r"F:\SPARK_DEFINITIVE_PROJECTS\\data\Spark-The-Definitive-Guide\data\flight-data\json\2015-summary.json")
+
+
+df.select(col("DEST_COUNTRY_NAME"))
+
+
+df.columns
+
+df.first()
+
+# pg  : 70
